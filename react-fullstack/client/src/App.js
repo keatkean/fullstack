c1c6555c6471647985bce6a739fcc0e189592f93
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-05 19:00:58 +0800

    display post list

--- react-fullstack/client/src/App.js
+++ react-fullstack/client/src/App.js
@@ -1,9 +1,29 @@
 //import logo from './logo.svg';
 //import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/post/list").then((res) => {
+      //console.log(res.data);
+      setPostList(res.data);
+    })
+  }, []);
+
   return (
     <div className="App">
+      {
+        postList.map((post, i) => {
+          return <div key={post.id}>
+            <div>{post.title}</div>
+            <div>{post.description}</div>
+            <div>{post.username}</div>
+          </div>;
+        })
+      }
     </div>
   );
 }
