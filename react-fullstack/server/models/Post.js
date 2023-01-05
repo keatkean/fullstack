dcaca5f5efdd2a53eb06b41389d66d8bcf0df413
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-05 17:02:23 +0800

    1 Introduction

---
+++ react-fullstack/server/models/Post.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+    const Post = sequelize.define("Post", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    return Post;
+}
