0c3c6cfaea8859043e58ddd60500c3182cc05354
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-09 16:55:56 +0800

    register and login pages

--- react-fullstack/client/src/App.js
+++ react-fullstack/client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home'
 import CreatePost from './pages/CreatePost';
 import Post from './pages/Post';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -10,8 +12,12 @@ function App() {
       <Router>
         <Link to="/">Home</Link>
         <Link to="/createPost">Create Post</Link>
+        <Link to="register">Register</Link>
+        <Link to="login">Login</Link>
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/register" exact element={<Register />} />
+          <Route path="/login" exact element={<Login />} />
           <Route path="/createPost" exact element={<CreatePost />} />
           <Route path="/post/:id" exact element={<Post />} />
         </Routes>
---
+++ react-fullstack/client/src/pages/Register.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+function Register() {
+    const navigate = useNavigate();
+    const [error, setError] = useState("");
+
+    const initialValues = {
+        username: "",
+        password: ""
+    };
+
+    const validationSchema = Yup.object().shape({
+        username: Yup.string().min(4).max(20).required(),
+        password: Yup.string().min(8).max(20).required()
+    });
+
+    const onSubmit = (data) => {
+        axios.post("/user/register", data).then((res) => {
+            if (res.data.error) {
+                setError(res.data.error);
+            }
+            else {
+                navigate("/login");
+            }
+        });
+    };
+
+    return (
+        <div>
+            <div>Register</div>
+            <Formik initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit} >
+                <Form>
+                    <div>
+                        <label>Username:</label>
+                        <Field name="username" />
+                        <ErrorMessage name="username" component="span" />
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <Field name="password" type="password" />
+                        <ErrorMessage name="password" component="span" />
+                    </div>
+                    <div>{error}</div>
+                    <button type="submit">Register</button>
+                </Form>
+            </Formik>
+        </div>
+    )
+}
+
+export default Register
