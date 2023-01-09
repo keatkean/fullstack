4132253b5bffc86518b69bb0432f1cbee515ac98
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-09 11:43:58 +0800

    display comment list

--- react-fullstack/client/src/pages/Post.js
+++ react-fullstack/client/src/pages/Post.js
@@ -6,20 +6,46 @@ import axios from 'axios';
 function Post() {
     let { id } = useParams();
     const [post, setPost] = useState({});
+    const [commentList, setCommentList] = useState([]);
 
     useEffect(() => {
         axios.get(`/post/details/${id}`).then((res) => {
             //console.log(res.data);
             setPost(res.data);
         });
+
+        axios.get(`/comment/list/${id}`).then((res) => {
+            //console.log(res.data);
+            setCommentList(res.data);
+        });
     }, []);
 
     return (
         <div>
-            <div>{post.title}</div>
-            <div>{post.description}</div>
-            <div>{post.username}</div>
-            <div>{post.createdAt}</div>
+            <div>
+                <div>{post.title}</div>
+                <div>{post.description}</div>
+                <div>{post.username}</div>
+                <div>{post.createdAt}</div><br/>
+            </div>
+
+            <div>
+                <div>
+                    Create Comment
+                </div><br/>
+                <div>
+                    {
+                        commentList.map((comment, i) => {
+                            return (
+                                <div key={comment.id}>
+                                    <div>{comment.text}</div>
+                                    <div>{comment.username}</div><br />
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
