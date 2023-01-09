cba1db8d71050cb708281419b4f87f79c59d5e82
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-09 16:49:30 +0800

    Update user.js

--- react-fullstack/server/routes/user.js
+++ react-fullstack/server/routes/user.js
@@ -5,6 +5,14 @@ const { User } = require('../models');
 
 router.post("/register", async (req, res) => {
     let { username, password } = req.body;
+    let user = await User.findOne({
+        where: { username: username }
+    });
+    if (user) {
+        res.json({ error: "Username already exists." });
+        return;
+    }
+
     let hash = await bcrypt.hash(password, 10);
     let result = await User.create({
         username: username,
