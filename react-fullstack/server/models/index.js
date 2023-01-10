d14a90aeba4f8f2ebdee23fe6f162172e404cc77
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-10 14:16:02 +0800

    use .env for db config

--- react-fullstack/server/models/index.js
+++ react-fullstack/server/models/index.js
@@ -5,16 +5,20 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+require('dotenv').config();
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// Create sequelize instance using config 
+let sequelize = new Sequelize(
+  process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD,
+  {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'mysql',
+      logging: false,
+      timezone: '+08:00'
+  }
+);
 
 fs
   .readdirSync(__dirname)
