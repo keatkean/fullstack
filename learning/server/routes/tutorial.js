7331c8b1e3969dd90631c73fceb023042c8b7a52
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-12 13:17:15 +0800

    update tutorial

--- learning/server/routes/tutorial.js
+++ learning/server/routes/tutorial.js
@@ -4,14 +4,23 @@ const { Tutorial, Sequelize } = require('../models');
 
 router.post("/", async (req, res) => {
     let tutorial = req.body;
-    let result = await Tutorial.create(tutorial);
-    res.json(result);
+
+    // Validate request
+    if (!tutorial.title) {
+        res.status(400).json({
+            message: "Content can not be empty."
+        });
+        return;
+    }
+
+    let data = await Tutorial.create(tutorial);
+    res.json(data);
 });
 
 router.get("/", async (req, res) => {
     let title = req.query.title;
     let condition = title ? { title: { [Sequelize.Op.like]: `%${title}%` } } : null;
-    
+
     let list = await Tutorial.findAll({
         where: condition,
         order: [['createdAt', 'DESC']]
@@ -23,6 +32,25 @@ router.get("/:id", async (req, res) => {
     let id = req.params.id;
     let tutorial = await Tutorial.findByPk(id);
     res.json(tutorial);
+});
+
+router.put("/:id", async (req, res) => {
+    let id = req.params.id;
+    let tutorial = req.body;
+
+    let num = await Tutorial.update(tutorial, {
+        where: { id: id }
+    });
+    if (num == 1) {
+        res.json({
+            message: "Tutorial was updated successfully."
+        });
+    }
+    else {
+        res.status(400).json({
+            message: `Cannot update tutorial with id=${id}.`
+        });
+    }
 });
 
 module.exports = router;
