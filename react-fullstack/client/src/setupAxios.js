eb99fd61718fbf87cf23834eb3c84fdebac42844
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-12 10:27:04 +0800

    use .env for api base url

--- react-fullstack/client/src/setupAxios.js
+++ react-fullstack/client/src/setupAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 function setupAxios() {
-    axios.defaults.baseURL = "http://localhost:3001";
+    axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 
     // Add a request interceptor
     axios.interceptors.request.use(function (config) {
