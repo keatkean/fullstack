e0e7a1eb3620a3e47e3d5213ef804bd5a80b3c46
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-12 11:37:09 +0800

    Update index.js

--- learning/server/index.js
+++ learning/server/index.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the learning space." });
+});
+
 const db = require('./models');
 db.sequelize.sync({ alter: true }).then(() => {
     let port = process.env.APP_PORT;
