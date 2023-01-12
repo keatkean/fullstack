8b3737a688f6df15eb227e94791c1889be27ce00
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-12 10:35:37 +0800

    add port to .env

--- react-fullstack/server/index.js
+++ react-fullstack/server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -15,7 +16,8 @@ app.use("/comment", commentRouter);
 
 const db = require('./models');
 db.sequelize.sync({ alter: true }).then(() => {
-    app.listen(3001, () => {
-        console.log("⚡ Sever running on http://localhost:3001");
+    let port = process.env.APP_PORT;
+    app.listen(port, () => {
+        console.log(`⚡ Sever running on http://localhost:${port}`);
     })
 });
