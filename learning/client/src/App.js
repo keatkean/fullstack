9d6e7b23911b8b535de06844052aff9a7901aed5
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-12 18:04:54 +0800

    Add AppBar

--- learning/client/src/App.js
+++ learning/client/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { Box, Container, AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Box>
+      <AppBar position="static" className='AppBar'>
+        <Container>
+          <Toolbar>
+            <Typography variant="h6" sx={{ mr: 2 }}>
+              Learning
+            </Typography>
+            <Button color="inherit">Tutorials</Button>
+            <Button color="inherit">Add</Button>
+            <Box sx={{ flexGrow: 1 }}>
+            </Box>
+            <Button color="inherit">Register</Button>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
   );
 }
 
