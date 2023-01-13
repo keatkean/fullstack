70cdd180bb95944ef605c5b6844c3bacb5a0d527
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-13 18:25:06 +0800

    use router

--- learning/client/src/App.js
+++ learning/client/src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
 import { Box, Container, AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Tutorials from './pages/Tutorials';
 
 function App() {
   return (
-    <Box>
-      <AppBar position="static" className='AppBar'>
-        <Container>
-          <Toolbar>
-            <Typography variant="h6" sx={{ mr: 2 }}>
-              Learning
-            </Typography>
-            <Button color="inherit">Tutorials</Button>
-            <Button color="inherit">Add</Button>
-            <Box sx={{ flexGrow: 1 }}>
-            </Box>
-            <Button color="inherit">Register</Button>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </Box>
+    <div>
+      <Router>
+        <AppBar position="static" className='AppBar'>
+          <Container>
+            <Toolbar>
+              <Link to="/">
+                <Typography variant="h6">
+                  Learning
+                </Typography>
+              </Link>
+              <Link to="/tutorials" >Tutorials</Link>
+              <Box sx={{ flexGrow: 1 }}>
+              </Box>
+              <Button color="inherit">Register</Button>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </Container>
+        </AppBar>
+
+        <Routes>
+          <Route path={"/"} element={<Tutorials />} />
+          <Route path={"/tutorials"} element={<Tutorials />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
