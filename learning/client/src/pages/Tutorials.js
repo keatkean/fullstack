e8960bc29b0cc2cea9a7a8cfb2fddbd9b5e01131
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-16 09:38:55 +0800

    use api to get tutorial list

--- learning/client/src/pages/Tutorials.js
+++ learning/client/src/pages/Tutorials.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import http from '../http';
 
 function Tutorials() {
-  return (
-    <div>Tutorials</div>
-  )
+    const [tutorialList, setTutorialList] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        http.get("/tutorial/").then((res) => {
+            //console.log(res.data);
+            setTutorialList(res.data);
+        });
+    }, []);
+
+    return (
+        <div>
+            <div>Tutorials</div><br />
+            {
+                tutorialList.map((post, i) => {
+                    return (
+                        <div key={post.id} onClick={() => { navigate(`/post/${post.id}`) }}>
+                            <div>{post.title}</div>
+                            <div>{post.description}</div>
+                            <br />
+                        </div>
+                    );
+                })
+            }
+        </div>
+    )
 }
 
 export default Tutorials
