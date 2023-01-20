ce8b08d6d61ae9c221ffa4c76a2de84d4c88e934
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-20 16:41:37 +0800

    new route

--- practical_01/learning-app/server/index.js
+++ practical_01/learning-app/server/index.js
@@ -4,11 +4,16 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 // Simple Route
 app.get("/", (req, res) => {
     res.send("Welcome to the learning space.");
 });
+
+// Routes
+const tutorialRoute = require('./routes/tutorial');
+app.use("/tutorial", tutorialRoute);
 
 let port = process.env.APP_PORT;
 app.listen(port, () => {
