b5d7c9be1513397098100a35d043b8b791eba853
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-01-25 19:49:07 +0800

    put function

--- practical_03/learning-app/server/routes/tutorial.js
+++ practical_03/learning-app/server/routes/tutorial.js
@@ -40,4 +40,28 @@ router.get("/:id", async (req, res) => {
     res.json(tutorial);
 });
 
+router.put("/:id", async (req, res) => {
+    let id = req.params.id;
+    let tutorial = await Tutorial.findByPk(id);
+    if (!tutorial) {
+        res.sendStatus(404);
+        return;
+    }
+    
+    let data = req.body;
+    let num = await Tutorial.update(data, {
+        where: { id: id }
+    });
+    if (num == 1) {
+        res.json({
+            message: "Tutorial was updated successfully."
+        });
+    }
+    else {
+        res.status(400).json({
+            message: `Cannot update tutorial with id ${id}.`
+        });
+    }
+});
+
 module.exports = router;
