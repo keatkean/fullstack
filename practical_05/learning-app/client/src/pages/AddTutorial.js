5a3575712da92e4818376d3fce3727d73a13dbd5
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-02-02 13:36:22 +0800

    Update AddTutorial.js

--- practical_05/learning-app/client/src/pages/AddTutorial.js
+++ practical_05/learning-app/client/src/pages/AddTutorial.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Box, Typography, TextField, Button } from '@mui/material';
 
 function AddTutorial() {
-  return (
-    <div>AddTutorial</div>
-  );
+    return (
+        <Box>
+            <Typography variant="h5" sx={{ my: 2 }}>
+                Add Tutorial
+            </Typography>
+            <Box component="form">
+                <TextField
+                    fullWidth margin="normal" autoComplete="off"
+                    label="Title"
+                    name="title"
+                />
+                <TextField
+                    fullWidth margin="normal" autoComplete="off"
+                    multiline minRows={2}
+                    label="Description"
+                    name="description"
+                />
+                <Box sx={{ mt: 2 }}>
+                    <Button variant="contained" type="submit">
+                        Add
+                    </Button>
+                </Box>
+            </Box>
+        </Box>
+    );
 }
 
 export default AddTutorial;
