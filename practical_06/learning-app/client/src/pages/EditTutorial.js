bb4b505702e23ea7116c32fdea102643976ab7cd
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-02-03 16:47:29 +0800

    formik

--- practical_06/learning-app/client/src/pages/EditTutorial.js
+++ practical_06/learning-app/client/src/pages/EditTutorial.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Typography, TextField, Button } from '@mui/material';
 import http from '../http';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 function EditTutorial() {
     const { id } = useParams();
@@ -18,22 +20,49 @@ function EditTutorial() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const formik = useFormik({
+        initialValues: tutorial,
+        enableReinitialize: true,
+        validationSchema: yup.object().shape({
+            title: yup.string()
+                .max(100, 'Title must be at most 100 characters')
+                .required('Title is required'),
+            description: yup.string()
+                .max(500, 'Description must be at most 500 characters')
+                .required('Description is required')
+        }),
+        onSubmit: (data) => {
+            http.put(`/tutorial/${id}`, data)
+                .then((res) => {
+                    console.log(res.data);
+                });
+        }
+    });
+
     return (
         <Box>
             <Typography variant="h5" sx={{ my: 2 }}>
                 Edit Tutorial
             </Typography>
-            <Box component="form">
+            <Box component="form" onSubmit={formik.handleSubmit}>
                 <TextField
                     fullWidth margin="normal" autoComplete="off"
                     label="Title"
                     name="title"
+                    value={formik.values.title}
+                    onChange={formik.handleChange}
+                    error={formik.touched.title && Boolean(formik.errors.title)}
+                    helperText={formik.touched.title && formik.errors.title}
                 />
                 <TextField
                     fullWidth margin="normal" autoComplete="off"
                     multiline minRows={2}
                     label="Description"
                     name="description"
+                    value={formik.values.description}
+                    onChange={formik.handleChange}
+                    error={formik.touched.description && Boolean(formik.errors.description)}
+                    helperText={formik.touched.description && formik.errors.description}
                 />
                 <Box sx={{ mt: 2 }}>
                     <Button variant="contained" type="submit">
