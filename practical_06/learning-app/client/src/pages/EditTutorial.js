016a97682162d3436727a4a2a9449798c785ec4d
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-02-03 16:22:54 +0800

    state

--- practical_06/learning-app/client/src/pages/EditTutorial.js
+++ practical_06/learning-app/client/src/pages/EditTutorial.js
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import http from '../http';
 
 function EditTutorial() {
     const { id } = useParams();
 
+    const [tutorial, setTutorial] = useState({
+        title: "",
+        description: ""
+    });
+
     useEffect(() => {
         http.get(`/tutorial/${id}`).then((res) => {
-            console.log(res.data);
+            setTutorial(res.data);
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
