202520108cd20283fcfeab463671d588e7d19abc
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-02-06 16:17:03 +0800

    copy from practical 6

---
+++ practical_07/learning-app/server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Simple Route
+app.get("/", (req, res) => {
+    res.send("Welcome to the learning space.");
+});
+
+// Routes
+const tutorialRoute = require('./routes/tutorial');
+app.use("/tutorial", tutorialRoute);
+
+const db = require('./models');
+db.sequelize.sync({ alter: true }).then(() => {
+    let port = process.env.APP_PORT;
+    app.listen(port, () => {
+        console.log(`⚡ Sever running on http://localhost:${port}`);
+    });
+});
