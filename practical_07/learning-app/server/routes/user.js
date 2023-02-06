68f1cdcae1dd350f7c51099f7fe0af84395b48b5
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-02-06 17:29:34 +0800

    check email

--- practical_07/learning-app/server/routes/user.js
+++ practical_07/learning-app/server/routes/user.js
@@ -5,6 +5,16 @@ const { User } = require('../models');
 
 router.post("/register", async (req, res) => {
     let data = req.body;
+
+    // Check email
+    let user = await User.findOne({
+        where: { email: data.email }
+    });
+    if (user) {
+        res.status(400).json({ message: "Email already exists." });
+        return;
+    }
+
     // Hash passowrd
     data.password = await bcrypt.hash(data.password, 10);
     // Create user
