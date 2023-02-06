8ea617bf7e8d7e7ecb329637693821414d954cb7
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-02-06 17:37:29 +0800

    validate request body

--- practical_07/learning-app/server/routes/user.js
+++ practical_07/learning-app/server/routes/user.js
@@ -2,9 +2,24 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const { User } = require('../models');
+const yup = require("yup");
 
 router.post("/register", async (req, res) => {
     let data = req.body;
+    // Validate request body
+    let validationSchema = yup.object().shape({
+        name: yup.string().min(3).max(50).required(),
+        email: yup.string().email().max(50).required(),
+        password: yup.string().min(8).max(50).required()
+    })
+    try {
+        await validationSchema.validate(data, { abortEarly: false });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(400).json({ errors: err.errors });
+        return;
+    }
 
     // Check email
     let user = await User.findOne({
