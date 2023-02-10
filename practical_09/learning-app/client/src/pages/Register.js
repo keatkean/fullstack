9bba58fca1a2d0eea8851bc4411790627d53cb0f
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-02-10 15:43:29 +0800

    add form

--- practical_09/learning-app/client/src/pages/Register.js
+++ practical_09/learning-app/client/src/pages/Register.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Box, Typography, TextField, Button } from '@mui/material';
 
 function Register() {
-  return (
-    <div>Register</div>
-  );
+    return (
+        <Box sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }}>
+            <Typography variant="h5" sx={{ my: 2 }}>
+                Register
+            </Typography>
+            <Box component="form" sx={{ maxWidth: '500px' }}>
+                <TextField
+                    fullWidth margin="normal" autoComplete="off"
+                    label="Name"
+                    name="name"
+                />
+                <TextField
+                    fullWidth margin="normal" autoComplete="off"
+                    label="Email"
+                    name="email"
+                />
+                <TextField
+                    fullWidth margin="normal" autoComplete="off"
+                    label="Password"
+                    name="password" type="password"
+                />
+                <TextField
+                    fullWidth margin="normal" autoComplete="off"
+                    label="Confirm Password"
+                    name="confirmPassword" type="password"
+                />
+                <Button fullWidth variant="contained" sx={{ mt: 2 }}
+                    type="submit">
+                    Register
+                </Button>
+            </Box>
+        </Box>
+    );
 }
 
 export default Register;
