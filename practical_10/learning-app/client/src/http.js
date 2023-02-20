e39545660a3356877ba09fb813172e5b85bc5067
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-02-20 16:42:06 +0800

    response incerceptor

--- practical_10/learning-app/client/src/http.js
+++ practical_10/learning-app/client/src/http.js
@@ -17,4 +17,19 @@ instance.interceptors.request.use(function (config) {
     return Promise.reject(error);
 });
 
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+}, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response.status === 401) {
+        localStorage.clear();
+        window.location = "/login";
+    }
+    return Promise.reject(error);
+});
+
 export default instance;
