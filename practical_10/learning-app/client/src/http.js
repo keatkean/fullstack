ff27be106ca44947c4842cf692f560abced63a1a
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-02-20 18:11:01 +0800

    Update http.js

--- practical_10/learning-app/client/src/http.js
+++ practical_10/learning-app/client/src/http.js
@@ -25,7 +25,7 @@ instance.interceptors.response.use(function (response) {
 }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    if (error.response.status === 401) {
+    if (error.response.status === 401 || error.response.status === 403) {
         localStorage.clear();
         window.location = "/login";
     }
