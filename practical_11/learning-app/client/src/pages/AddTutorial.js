04ca0809264cd3809ca9122464c49128d2ec8320
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-02-22 12:55:20 +0800

    add grid

--- practical_11/learning-app/client/src/pages/AddTutorial.js
+++ practical_11/learning-app/client/src/pages/AddTutorial.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Typography, TextField, Button } from '@mui/material';
+import { Box, Typography, TextField, Button, Grid } from '@mui/material';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import http from '../http';
@@ -36,25 +36,37 @@ function AddTutorial() {
                 Add Tutorial
             </Typography>
             <Box component="form" onSubmit={formik.handleSubmit}>
-                <TextField
-                    fullWidth margin="normal" autoComplete="off"
-                    label="Title"
-                    name="title"
-                    value={formik.values.title}
-                    onChange={formik.handleChange}
-                    error={formik.touched.title && Boolean(formik.errors.title)}
-                    helperText={formik.touched.title && formik.errors.title}
-                />
-                <TextField
-                    fullWidth margin="normal" autoComplete="off"
-                    multiline minRows={2}
-                    label="Description"
-                    name="description"
-                    value={formik.values.description}
-                    onChange={formik.handleChange}
-                    error={formik.touched.description && Boolean(formik.errors.description)}
-                    helperText={formik.touched.description && formik.errors.description}
-                />
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={6} lg={8}>
+                        <TextField
+                            fullWidth margin="normal" autoComplete="off"
+                            label="Title"
+                            name="title"
+                            value={formik.values.title}
+                            onChange={formik.handleChange}
+                            error={formik.touched.title && Boolean(formik.errors.title)}
+                            helperText={formik.touched.title && formik.errors.title}
+                        />
+                        <TextField
+                            fullWidth margin="normal" autoComplete="off"
+                            multiline minRows={2}
+                            label="Description"
+                            name="description"
+                            value={formik.values.description}
+                            onChange={formik.handleChange}
+                            error={formik.touched.description && Boolean(formik.errors.description)}
+                            helperText={formik.touched.description && formik.errors.description}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={6} lg={4}>
+                        <Box sx={{ textAlign: 'center', mt: 2 }} >
+                            <Button variant="contained" component="label">
+                                Upload Image
+                                <input hidden accept="image/*" multiple type="file" />
+                            </Button>
+                        </Box>
+                    </Grid>
+                </Grid>
                 <Box sx={{ mt: 2 }}>
                     <Button variant="contained" type="submit">
                         Add
