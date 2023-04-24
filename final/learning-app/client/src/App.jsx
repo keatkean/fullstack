e635d604bf8be441722db65bd253e76bac264d3d
Li Haiyun <li_haiyun@nyp.edu.sg>
2023-04-24 17:51:32 +0800

    add solution

--- final/learning-app/client/src/App.jsx
+++ final/learning-app/client/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+import { Container, AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Tutorials from './pages/Tutorials';
+import AddTutorial from './pages/AddTutorial';
+import EditTutorial from './pages/EditTutorial';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import http from './http';
+import UserContext from './contexts/UserContext';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      http.get('/user/auth').then((res) => {
+        setUser(res.data.user);
+      });
+    }
+  }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    window.location = "/";
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <UserContext.Provider value={{ user, setUser }}>
+      <Router>
+        <AppBar position="static" className="AppBar">
+          <Container>
+            <Toolbar disableGutters={true}>
+              <Link to="/">
+                <Typography variant="h6" component="div">
+                  Learning
+                </Typography>
+              </Link>
+              <Link to="/tutorials" ><Typography>Tutorials</Typography></Link>
+              <Box sx={{ flexGrow: 1 }}></Box>
+              {user && (
+                <>
+                  <Typography>{user.name}</Typography>
+                  <Button onClick={logout}>Logout</Button>
+                </>
+              )
+              }
+              {!user && (
+                <>
+                  <Link to="/register" ><Typography>Register</Typography></Link>
+                  <Link to="/login" ><Typography>Login</Typography></Link>
+                </>
+              )}
+            </Toolbar>
+          </Container>
+        </AppBar>
+
+        <Container>
+          <Routes>
+            <Route path={"/"} element={<Tutorials />} />
+            <Route path={"/tutorials"} element={<Tutorials />} />
+            <Route path={"/addtutorial"} element={<AddTutorial />} />
+            <Route path={"/edittutorial/:id"} element={<EditTutorial />} />
+            <Route path={"/register"} element={<Register />} />
+            <Route path={"/login"} element={<Login />} />
+          </Routes>
+        </Container>
+      </Router>
+    </UserContext.Provider>
+  );
 }
 
-export default App
+export default App;
